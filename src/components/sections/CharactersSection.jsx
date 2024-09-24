4843d5bc806e8edd0e6bdcdcc26958158d55c233
Aritz <aritzsilva98@gmail.com>
2024-09-25 01:57:01 +0200

    Tried to fix Location Title in mobile view

--- src/components/sections/CharactersSection.jsx
+++ src/components/sections/CharactersSection.jsx
@@ -10,7 +10,7 @@ const CharactersSection = () => {
     <section className='characters-section pl-4'>
       {hasMyCharacters && (
         <div className='bg-amber-50 border-2 border-amber-700 rounded-lg p-6 shadow-lg mb-8'>
-          <h2 className='text-xl font-bold mb-4 text-center sm:text-2xl md:text-3xl lg:text-4xl'>Personajes Jugadores</h2>
+          <h2 className='text-xl font-bold mb-4 text-center sm:text-2xl sm:text-center md:text-3xl md:text-left lg:text-4xl'>Personajes Jugadores</h2>
           <div className='grid grid-cols-1 md:grid-cols-2 gap-8 pb-4'>
             {myCharacters.map((character, index) => (
               <CharacterItem key={character.id || `${character.name}-${index}`} character={character} />
@@ -23,7 +23,7 @@ const CharactersSection = () => {
 
       {hasCharacters && (
         <div className='bg-amber-50 border-2 border-amber-700 rounded-lg p-6 shadow-lg mb-8'>
-          <h2 className='text-2xl font-bold mb-4 text-center'>Personajes No Jugadores</h2>
+          <h2 className='text-xl font-bold mb-4 text-center sm:text-2xl sm:text-center md:text-3xl md:text-left lg:text-4xl'>Personajes No Jugadores</h2>
           <div className='grid grid-cols-1 md:grid-cols-2 gap-8 pb-4'>
             {characters.map((character, index) => (
               <CharacterItem key={character.id || `${character.name}-${index}`} character={character} />
--- src/components/sections/LocationsSection.jsx
+++ src/components/sections/LocationsSection.jsx
@@ -6,7 +6,7 @@ const LocationsSection = () => {
     <section className='locations-section pl-4'>
       {locations?.length > 0 ? (
         <div className='bg-amber-50 border-2 border-amber-700 rounded-lg p-6 shadow-lg mb-8'>
-          <h2 className='text-4xl font-bold mb-4'>Localizaciones</h2>
+          <h2 className='text-xl font-bold mb-4 text-center sm:text-2xl md:text-3xl lg:text-4xl'>Localizaciones</h2>
           <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
             {locations.map(location => (
               <LocationItem key={location.name} location={location} />
